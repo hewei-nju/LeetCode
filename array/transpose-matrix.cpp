94f9820909da285a7d427ede9c52c384ce323e78
hewei <191250044@smail.nju.edu.cn>
2021-03-24 20:50:14 +0800

    2021-03-24

---
+++ array/transpose-matrix.cpp
@@ -0,0 +1,16 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> res (n, vector<int>(m))
+        for (int j = 0; j <= matrix[0].size() - 1; ++j) {
+            for (int i = 0; i <= matrix.size() - 1; ++i) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+};
