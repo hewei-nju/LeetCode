994111d51efb39c670e0d46e792f7be8ea4d11dc
hewei <191250044@smail.nju.edu.cn>
2021-03-25 15:15:24 +0800

    push

---
+++ src/isPalindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        } else {
+            int reverse_num = 0;
+            while (x > reverse_num) {
+                reverse_num = reverse_num * 10 + x % 10;
+                x /= 10;
+            }
+            return x == reverse_num || x == reverse_num / 10;
+        }
+    }
+};
--- src/reverse.cpp
+++ src/reverse.cpp
@@ -1,19 +1,21 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 class Solution {
 public:
     int reverse(int x) {
-        if (x == INT32_MIN) {
-            return 0;
-        } else {
-            if (x < 0) {
-                printf("-");
-                stringstream s;
-                s << -1 * x;
-                string s = s.str();
-            
+        int ans = 0;
+        while (x != 0) {
+            int tmp = x % 10;
+            x /= 10;
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && tmp > 7)) {
+                return 0;
+            } else if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && tmp < -8)) {
+                return 0;
+            } else {
+                ans = ans * 10 + tmp;
             }
         }
+        return ans;
     }
 };
