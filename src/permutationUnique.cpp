e8c2ef6dd295012274edc511538d91cc8f43251d
hewei <191250044@smail.nju.edu.cn>
2021-04-28 10:58:58 +0800

    push

---
+++ src/permutationUnique.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int> res;
+        if (nums.empty()) {
+            return res;
+        }
+        vector<int> res(nums.size(), -11);
+    }
+    vector<vector<int>> permutation(vector<int> &nums, vector<int>&res) {
+        for (int i = 0; i <= nums.size() - 1; ++i) {
+            vec
+        }
+    }
+};
+
+
+////  ! 
