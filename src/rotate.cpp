69cea9dff027524775207ba84f2777e738b5f936
hewei <191250044@smail.nju.edu.cn>
2021-05-12 13:19:19 +0800

    push

--- src/rotate.cpp
+++ src/rotate.cpp
@@ -1,6 +1,8 @@
-class Solution {
+class Solution
+{
 public:
-    void rotate(vector<int>& nums, int k) {
+    void rotate(vector<int> &nums, int k)
+    {
         if (nums.size() == 0)
             return;
         k = k % nums.size();
@@ -8,23 +10,24 @@ public:
         reverse(nums, 0, k - 1);
         reverse(nums, k, nums.size() - 1);
     }
-        /**3 翻转，这个我是抄的题解*/
-    void reverse(vector<int>& nums, int start, int end) {
-        while (start < end) {
+    /**3 翻转，这个我是抄的题解*/
+    void reverse(vector<int> &nums, int start, int end)
+    {
+        while (start < end)
+        {
             swap(nums[start], nums[end]);
             start += 1;
             end -= 1;
         }
     }
-        /* 2
+    /* 2
         vector<int> tmp;
         for (int i = nums.size() - k; i <= nums.size() - 1; ++i)
             tmp.push_back(nums[i]);
-        for (int i = 0; i < k; ++i)
-            tmp.push_back(nums[i]);
         for (int i = 0; i < nums.size() - k; ++i)
+            tmp.push_back(nums[i]);
+        for (int i = 0; i <= nums.size() - 1; ++i)
             nums[i] = tmp[i];
-        */
         /* 1
             while (k > 0) {
             --k;
@@ -34,5 +37,6 @@ public:
             }
             nums[0] = tmp;
         }*/
-    }
-};
+}
+}
+;
