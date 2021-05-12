505eb58728351eb0ef95ee107ad3e78a1b567212
hewei <191250044@smail.nju.edu.cn>
2021-05-12 11:29:20 +0800

    push

---
+++ src/rotate.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if (nums.size() == 0)
+            return;
+        k = k % nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
+    }
+        /**3 翻转，这个我是抄的题解*/
+    void reverse(vector<int>& nums, int start, int end) {
+        while (start < end) {
+            swap(nums[start], nums[end]);
+            start += 1;
+            end -= 1;
+        }
+    }
+        /* 2
+        vector<int> tmp;
+        for (int i = nums.size() - k; i <= nums.size() - 1; ++i)
+            tmp.push_back(nums[i]);
+        for (int i = 0; i < k; ++i)
+            tmp.push_back(nums[i]);
+        for (int i = 0; i < nums.size() - k; ++i)
+            nums[i] = tmp[i];
+        */
+        /* 1
+            while (k > 0) {
+            --k;
+            int tmp = nums[nums.size() - 1];
+            for (int i = nums.size(); i >= 1; --i) {
+                nums[i] = nums[i - 1];
+            }
+            nums[0] = tmp;
+        }*/
+    }
+};
