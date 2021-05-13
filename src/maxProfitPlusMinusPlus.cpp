58af8a9d493ecb9d7616c7674e0a08ee3deb8a8a
hewei <191250044@smail.nju.edu.cn>
2021-05-14 00:57:00 +0800

    2021-05-14

--- src/maxProfitPlusMinusPlus.cpp
+++ src/maxProfitPlusMinusPlus.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int buy = INT_MIN, sell = 0, freeze = 0;
+        for (auto &p: prices) {
+            buy = max(buy, freeze - p);
+            freeze = sell;  // 注意freeze和sell的顺序！
+            sell = max(sell, buy + p);
+        }
+        return sell;  
+    }
+    /**
+    int maxProfit(vector<int>& prices) {
         int buy = INT_MIN, sell = 0, lock = 0;
         for (auto &p: prices) {
             buy = max(buy, lock - p);
@@ -9,4 +19,6 @@ public:
         }
         return sell;
     }
+    */
+
 };
