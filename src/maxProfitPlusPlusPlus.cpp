c3b0805a858fc327ddc8ad2ca2aa78a6430a78ef
hewei <191250044@smail.nju.edu.cn>
2021-05-13 19:47:29 +0800

    2021-05-13

---
+++ src/maxProfitPlusPlusPlus.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        vector<int> buy(k + 1, INT_MIN);
+        vector<int> sell(k + 1, 0);
+        for (auto &price: prices) {
+            for (int i = 1; i <= k; ++i) {
+                buy[i] = max(buy[i], sell[i - 1] - price);
+                sell[i] = max(sell[i], buy[i] + price);
+            }
+        }
+        return sell[k];
+    }
+};
