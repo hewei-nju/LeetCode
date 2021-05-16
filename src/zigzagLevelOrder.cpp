a3b196fe9c02160ada387c8e221552279a2d121e
hewei <191250044@smail.nju.edu.cn>
2021-05-16 11:09:17 +0800

    2021-05-16

--- src/zigzagLevelOrder.cpp
+++ src/zigzagLevelOrder.cpp
@@ -12,42 +12,39 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        deque<TreeNode *> deq;
-        queue<TreeNode *> que;
+        stack<TreeNode *> stk1;
+        stack<TreeNode *> stk2;
         bool tag = true;
         vector<vector<int>> ret;
-        if (root == nullptr) {
+        if (root == nullptr)
             return ret;
-        }
-        deq.push_back(root);
-        //vector<int> tmp{deq1.front()->val};
-        //tag = false;
-        //ret.push_back(tmp);
-        TreeNode *last = new TreeNode;
-        TreeNode *next = nullptr;
-        last->left = root;
-        last->right = nullptr;
-
-        while (!deq.empty()) {
-            vector<int> vec;
-            while (tag && !deq.empty()) {
-                next = deq.front();
-                if (last->left == nullptr && last->next == nullptr) {
-                    last = next;
-                }
-                if (last->left == next || last->right == next) {
-                    tag = false;
-                    break;
-                }
-                vec.push_back(next->val);
-                que.push(next);
-                deq.pop_front();
+        stk1.push(root);
+        TreeNode *top = nullptr;
+        while (!stk1.empty() || !stk2.empty()) {
+            vector<int> vec1, vec2;
+            while (!stk1.empty()) {
+                top = stk1.top();
+                stk1.pop();
+                vec1.push_back(top->val);
+                if (top->left != nullptr)
+                    stk2.push(top->left);
+                if (top->right != nullptr)
+                    stk2.push(top->right);
             }
-            while (!tag) {
-                next = deq.back();
+            if (!vec1.empty())
+                ret.push_back(vec1);
+            while (!stk2.empty()) {
+                top = stk2.top();
+                stk2.pop();
+                vec2.push_back(top->val);
+                if (top->right != nullptr)
+                    stk1.push(top->right);
+                if (top->left != nullptr)
+                    stk1.push(top->left);
             }
-            vector<int> vec;
-            
+            if (!vec2.empty())
+                ret.push_back(vec2);
         }
+        return ret;
     }
 };
