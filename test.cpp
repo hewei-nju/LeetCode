9a0de922eabaa31a510f636503664690f45c66df
hewei <191250044@smail.nju.edu.cn>
2021-05-21 15:14:10 +0800

    2021-05-21

--- test.cpp
+++ test.cpp
@@ -9,6 +9,9 @@ int main()
 {
     if (isalpha('1'))
         cout << (char)tolower('s') << endl;
+    vector<int> nums{1, 2};
+    int *arr = new int[2]{nums};
+    cout << *arr << " " << *(arr + 1) << endl;
 }
 
 
